958e7c90b1cb39ba69517b83853b916a143f9b35
chaoyp827605855 <40753831+chaoyp827605855@users.noreply.github.com>
2018-07-02 21:12:32 +0800

    ------版本冲突实验 BBBB------

--- src/test/java/easyUI/EasyUITest.java
+++ src/test/java/easyUI/EasyUITest.java
@@ -8,5 +8,7 @@ public class EasyUITest {
     public void fun1() {
         System.out.println("----fun1()分支合并-----");
     }
-
+    public void bb() {
+        System.out.println("------版本冲突实验 BBBB------");
+    }
 }
