5323d80f45909833371e3e89bdb20af24a59d3ee
chao <827605855@qq.com>
2018-07-02 21:07:48 +0800

    dev分支的fun2() 方法

--- src/test/java/easyUI/EasyUITest.java
+++ src/test/java/easyUI/EasyUITest.java
@@ -5,18 +5,8 @@ package easyUI;
  */
 public class EasyUITest {
 
-    public void fun1(){
+    public void fun1() {
         System.out.println("----fun1()分支合并-----");
-        System.out.println("++++++++++++++++++++++");
-        System.out.println("AAAAAAAAAAAAAAAAAAAAA");
-    }
-
-    public void fun2(){
-        System.out.println("----dev 分支-----");
-    }
-
-    public void fun3(){
-        System.out.println("----deadasdasv 分支-----");
     }
 
 }
